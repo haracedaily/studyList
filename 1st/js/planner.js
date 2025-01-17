1f7eb356ba2049e1ac012abdeaf9d41ba4510e22
준호 <123717055+haracedaily@users.noreply.github.com>
2025-01-17 13:16:05 +0900

    Add files via upload

--- 1st/js/planner.js
+++ 1st/js/planner.js
@@ -1,4 +1,9 @@
 function limit(e){
+    if(e.value.length>1){
+        e.parentNode.style.backgroundColor='#F1EDFF';
+    }else{
+        e.parentNode.style.backgroundColor='#fff';
+    }
     if(e.value.split('\n').length>3){
         swal('허용되지 않은 입력','3줄까지만 입력 가능합니다.','warning',{
             button:'돌아가기',
@@ -7,13 +12,3 @@ function limit(e){
     }
     
 }
-
-/* $('#contents').keydown(function(){
-    var rows = $('#contents').val().split('\n').length;
-    var maxRows = 4;
-    if( rows > maxRows){
-        alert('4줄 까지만 가능합니다');
-        modifiedText = $('#contents').val().split("\n").slice(0, maxRows);
-        $('#contents').val(modifiedText.join("\n"));
-    }
-}); */
