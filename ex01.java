c8992a29efceada31b3ad92d04ab254e23ecd4e1
준호 <123717055+haracedaily@users.noreply.github.com>
2025-01-22 11:57:09 +0900

    Add files via upload

---
+++ ex01.java
@@ -0,0 +1,7 @@
+class Ex01{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+    }
+}
---
+++ ex01.js
@@ -0,0 +1,6 @@
+console.log("Hello javascript")
+
+var a = 10;
+var b = 20;
+console.log(a+b);
+
