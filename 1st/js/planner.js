d41608053a2b61971d618c1ac38c96cf7a63f925
준호 <123717055+haracedaily@users.noreply.github.com>
2025-01-22 11:56:26 +0900

    Add files via upload

--- 1st/js/planner.js
+++ 1st/js/planner.js
@@ -16,10 +16,10 @@ class current_color {
   }
 var item;
 var todoItem;
-
+let current_who = new current_color('purple','#F1EDFF')
 document.addEventListener("DOMContentLoaded", function (e) {
-    current_color.color='purple';
-     current_color.code='#F1EDFF';
+/*     current_color.color='purple';
+     current_color.code='#F1EDFF'; */
     var noteItems = document.getElementsByClassName("dd");
     for (let i = 0; i < noteItems.length; i++) {
         var item = noteItems[i];
@@ -46,8 +46,8 @@ function chk(e) {
         input.className = "li_content";
         input.addEventListener("keyup", function () {
             chk(input);
-
         })
+        input.maxLength="24";
         li.appendChild(input);
         document.getElementById("to_do_ul").appendChild(li);
         console.log(document.getElementById("to_do_ul").lastChild);
@@ -79,7 +79,7 @@ function hideDate() {
 
 function limit(e) {
     if (e.value.length > 1) {
-        e.parentNode.style.backgroundColor = current_color.code;
+        e.parentNode.style.backgroundColor = current_who.getCode();
     } else {
         e.parentNode.style.backgroundColor = '#f8f8f8';
     }
@@ -95,26 +95,26 @@ function limit(e) {
 
 function changeBackColor(a, b){
     if(b=='pastel'){
-        current_color.color='pastel';
-        current_color.code='#fff0e8';
+        current_who.color='pastel';
+        current_who.code='#fff0e8';
 document.getElementById("planner_date").style.backgroundColor="#fff0e8";
 document.getElementById("to_do_list").style.backgroundColor="#ffd7c2";
 document.getElementById("for_tomorrow").style.backgroundColor="#fff0e8";
     }else if(b=='purple'){
-        current_color.color='purple';
-        current_color.code='#F1EDFF';
+        current_who.color='purple';
+        current_who.code='#F1EDFF';
         document.getElementById("planner_date").style.backgroundColor="#F1EDFF";
         document.getElementById("to_do_list").style.backgroundColor="#c4b4ff";
         document.getElementById("for_tomorrow").style.backgroundColor="#F1EDFF";
     }else if(b=='blue'){
-        current_color.color='blue';
-        current_color.code='#dee5fe';
+        current_who.color='blue';
+        current_who.code='#dee5fe';
         document.getElementById("planner_date").style.backgroundColor="#dee5fe";
         document.getElementById("to_do_list").style.backgroundColor="#c3d1ff";
         document.getElementById("for_tomorrow").style.backgroundColor="#dee5fe";
     }else{
-        current_color.color='green';
-        current_color.code='#C5FFE8';
+        current_who.color='green';
+        current_who.code='#C5FFE8';
         document.getElementById("planner_date").style.backgroundColor="#C5FFE8";
         document.getElementById("to_do_list").style.backgroundColor="#62EEB6";
         document.getElementById("for_tomorrow").style.backgroundColor="#C5FFE8";
