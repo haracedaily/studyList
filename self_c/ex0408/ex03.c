389e50a241003ffdc854f87808b504cb2dfa0bfc
준호 <123717055+haracedaily@users.noreply.github.com>
2025-04-08 21:53:07 +0900

    Create ex03.c

---
+++ self_c/ex0408/ex03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <float.h>
+
+int main(){
+    char c1 = 'F';
+    char c2 = '%';
+    
+    
+    printf("%c %d\n",c1,c1);
+    printf("%c %d\n",c2,c2);
+    return 0;
+}
