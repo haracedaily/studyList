bf556b98d823aea1f1a3ae955ef3c9d48a79f183
준호 <123717055+haracedaily@users.noreply.github.com>
2025-04-10 21:12:32 +0900

    Create ex02.c

---
+++ self_c/ex0410/ex02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int num1, num2;
+    num1 = 20;
+     num2=10;
+      printf("%d\n",num1<num2||num1>num2);
+      printf("%d\n",num1<num2&&num1>num2);
+    
+    return 0;
+}
