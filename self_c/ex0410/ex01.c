b5cb8498f806e8e1df7f87e0c82e91b02a6d0b7b
준호 <123717055+haracedaily@users.noreply.github.com>
2025-04-10 21:10:21 +0900

    Create ex01.c

---
+++ self_c/ex0410/ex01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int num1, num2;
+    num1 = 20;
+     num2=10;
+      if(num1==20){
+          if(num2==10){
+              printf("참\n");
+          }
+          else
+          printf("false\n");
+      }
+      else{
+          printf("false\n");
+      }
+    
+    return 0;
+}
