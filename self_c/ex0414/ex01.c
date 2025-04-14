9550cd68f93ecdbea6a96fa6e6f86109ce74c2ea
준호 <123717055+haracedaily@users.noreply.github.com>
2025-04-14 20:00:09 +0900

    Update ex01.c

--- self_c/ex0414/ex01.c
+++ self_c/ex0414/ex01.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(){
+    char *charPtr;
+    double *doublePtr;
     int *numeric;
     int num1;
     num1 = 4;
@@ -9,6 +11,8 @@ int main(){
     printf("%p\n",&num1);
     printf("%p\n",numeric+1);
     printf("numeric의 크기 = %d Byte\n",sizeof numeric);
+    printf("charPtr의 크기 = %d Byte\n",sizeof charPtr);
+    printf("doublePtr의 크기 = %d Byte\n",sizeof doublePtr);
     
     return 0;
 }
