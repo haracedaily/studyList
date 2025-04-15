abb4c2301cb39dd356646bcbca563b0b401e440f
준호 <123717055+haracedaily@users.noreply.github.com>
2025-04-15 21:23:40 +0900

    Update ex02.c

--- self_c/ex0415/ex02.c
+++ self_c/ex0415/ex02.c
@@ -7,9 +7,10 @@ int main(){
     printf("%c \n",s1[1]);
     printf("%c \n",s1[3]);
     printf("%c \n",s1[4]);
-    //s1[0]='A';//리터럴값은 수정할 수 없다.
+    //s1[0]='A';//포인터의 리터럴값은 수정할 수 없다.
     
     char s2[] = "Hello!! c";
+    s2[0] = 'A';//포인터와 다르게 배열은 대입 가능
     printf("%s\n",s2);
     printf("%c\n",s2[9]);
     printf("s2의 크기 : %d byte\n",sizeof s2);//마지막값 NULL or 0이 들어가야함
