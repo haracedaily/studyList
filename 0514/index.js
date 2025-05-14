a5e8432d2c7020a8e0c42d0083c796b5f52738e0
DESKTOP-29VI7GO\user <nj7001@naver.com>
2025-05-14 12:45:53 +0900

    no message

--- 0514/index.js
+++ 0514/index.js
@@ -5,15 +5,26 @@ const fs = require("fs");
 
 
 http.createServer(async (req,res)=>{
-    console.log(req.url);
-    try{
-if(req.url==='/'){
-    const password = "비밀";
-    const indexHtml = await fs.promises.readFile("./index.html");
+try{
+    if(req.url==='/'){
+        const password = "비밀";
+        const indexHtml = await fs.promises.readFile("./index.html");
+        res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'});
+        res.end(indexHtml);
+}else if(req.url==='/join' && req.method==="POST"){
+    req.setEncoding('utf-8');
+    let body = '';
+        req.on('data', (data)=>{
+            body += data;
+        })
+        req.on('end', () => {
+            console.log(body);
+            const{id,password} = JSON.parse(body);
+            console.log(id,password);
+        })
 
-    res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'});
-    res.end(indexHtml);
-}else if(req.url === '/join'){
+    console.log(body);
+
     const obj = {name:"홍길동",age:20};
     res.writeHead(200,{'Content-Type':'application/json; charset=utf-8'});
     res.end(JSON.stringify(obj));
@@ -21,6 +32,9 @@ if(req.url==='/'){
     res.writeHead(200,{'Content-Type':'text/plain; charset=utf-8'});
     res.end('로그인 성공');
 }
+    res.writeHead(404,{'Content-Type':'text/plain;'});
+    res.end('잘못된 경로입니다.');
+
 /*
         fs에 promises사용
         fs.readFileSync("./index.html",'utf-8',(err,data)=>{
@@ -32,7 +46,7 @@ if(req.url==='/'){
         res.writeHead(505,{'Content-Type':'text/plain; charset=utf-8'});
         res.end(error.message);
     }
-}).listen(8080,'0.0.0.0',()=>{
+}).listen(8080,()=>{
     /*0.0.0.0은 어떠한 경로에도 8080포트를 사용하면 허용*/
     console.log("listening to port 8080");
 })
