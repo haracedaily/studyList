ea8c9358a7973df170f4e632b615317ca52a78bc
DESKTOP-29VI7GO\user <nj7001@naver.com>
2025-05-21 12:42:30 +0900

    no message

--- node_work/rest03/app.js
+++ node_work/rest03/app.js
@@ -3,15 +3,27 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const morgan = require("morgan"); // (req,res,next=>{}) 미들웨어'
+const cookieParser = require("cookie-parser");
+
+console.log(process.env.COOKIE_SECRET);
 
 app.use(morgan("dev")); // 미들웨어 등록
 // console.log(morgan("dev").toString());
+// static에서는 해당 경로에서 해당하는 파일이 있으면 클라이언트에게 준다.
+// 클라이언트가 요청하는 uri, 해당 경로
+// ex) app.use("/img", express.static(path.join(__dirname, "public")));
+// 기존 url/파일명 => 리턴 : /public/파일명
 app.use("/", express.static(path.join(__dirname, "public")));
 // console.log(express.static(path.join(__dirname, "public")).toString());
+// req.body 파리메타 받아줌 { id:"aaa@naver.com"}
 app.use(express.json());
+// req.query ?aa=10&bb=20;
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 app.use((req, res, next) => {
+    // console.log(req.body);
+    // console.log(req.query);
     console.log("모든 요청은 여기 들렸다가 진행된다.");
     next();
 });
@@ -38,6 +50,27 @@ app.get(
         // res.json('이거 두번 보내기 되냐');
     }
 );
+app.get("/getCoo",(req,res,next)=>{
+    console.log("쿠키 : ",req.cookies);
+    console.log("암호쿠키 : ",req.signedCookies);
+    res.send("저기옴");
+})
+app.get("/setCoo",(req,res,next)=>{
+    console.log("test");
+    res.cookie("name","test",{
+        expires:new Date(Date.now()+1000*5),
+        secure:true,
+        signed:true,
+        httpOnly:true
+    });
+    // res.cookie("name","test",{signed:true, httpOnly:true});
+    // expires 유효시간
+    // httpOnly http외에선 사용 막기
+    // secure 보안상 막음
+    // signed 암호화
+    res.send("여기옴?");
+})
+
 app.post("/", async (req, res) => {
     const conn = await pool.getConnection(); // 연결 객체 가져오기
     const result = await conn.execute(`insert into users 
@@ -47,18 +80,41 @@ app.post("/", async (req, res) => {
     conn.release(); // 연결 객체 반환
     res.send(result);
 });
-app.put("/", (req, res) => {
-    res.send("Hello put");
+app.put("/", async (req, res) => {
+    console.log(req.body);
+    console.log(req.query);
+    if(req.body.idx||req.query.idx){
+        const idx = req.body.idx||req.query.idx;
+        const id = req.body.id||req.query.id;
+        const password = req.body.password||req.query.password;
+        console.log(idx,id,password);
+    const conn = await pool.getConnection();
+    const result = await conn.execute("update users set id = ? , password = ? where idx = ?",[id,password,idx]);
+    conn.release();
+    res.send(result);
+    }else{
+        res.send("idx값이 빠졌습니다.")
+    }
+
+
 });
-app.delete("/", (req, res) => {
-    throw new Error("강제에러 발생");
-    res.send("hello delete");
+app.delete("/", async (req, res) => {
+    if(req.body.idx||req.query.idx){
+        const idx = req.body.idx||req.query.idx;
+        const conn = await pool.getConnection();
+        const result = await conn.execute("delete from users where idx = ?",[idx]);
+        conn.release();
+        res.send(result);
+    }else{
+    res.send("프론트님 idx 값을 넘겨주세요");
+    }
 });
 
 app.get("/html", (req, res) => {
     res.sendFile(path.join(__dirname, "./index.html"));
 });
 
+// 모든 에러는 여기로 진행된다
 app.use((err, req, res, next) => {
     console.log(err);
     res.status(500).send("에러가발생하였습니다.");
