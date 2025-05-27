3634abd1a9cea0b97c9ea6f93bd19b0d2ac16037
DESKTOP-29VI7GO\user <nj7001@naver.com>
2025-05-27 11:09:47 +0900

    no message

--- node_work/back02/app.js
+++ node_work/back02/app.js
@@ -35,6 +35,58 @@ app.use((req,res,next)=>{
     next();
 })
 
+//서버 시작 누르게 되면 배열 값 초기화
+//배열이기 때문에 서버 재시작하면 프론트 정보 사라짐
+//DB에 저장 필수.
+const ss = [];
+
+app.post("/subscribe",(req,res,next)=>{
+    console.log(req.body);
+    ss.push({sub:req.body});
+    console.log(ss);
+    res.send("구독성공");
+});
+
+app.get("/send", async (req, res, next) => {
+    console.log("푸시알림 보내기");
+    /* const sub = ss[0].sub;
+    const payload = JSON.stringify({
+        title: "테스트",
+        body: "테스트 내용",
+        icon: "https://www.google.com/favicon.ico"
+    });
+//보통 명시적으로 payload를 지정해주고 보내는게 일반적
+    // 푸시 알림 전송
+    webpush.sendNotification(sub, payload)
+        .then(() => {
+            console.log("푸시 알림 전송 성공");
+            res.send("푸시 알림 전송 성공");
+        })
+        .catch(err => {
+            console.error("푸시 알림 전송 실패", err);
+            res.status(500).send("푸시 알림 전송 실패");
+        }); */
+        try {
+            const payload = JSON.stringify({
+                title: "새로운 알림",
+                body: "새로운 정보가 도착하였습니다.",
+                url: "https://front02-two.vercel.app/",
+            });
+            const notifications = ss.map( item => {
+                console.log("item : ",item);
+                return webpush.sendNotification(item.sub, payload);
+            });
+            console.log("notifications : ",notifications);
+            // 모든 푸시 알림 전송을 병렬로 처리
+            await Promise.all(notifications);
+            res.json({message:"푸시알람 전성 성공"})
+        } catch (error) {
+            console.error("에러 발생:", error);
+
+            res.status(500).json({message:"푸시알람 전송 실패"});
+        }
+});
+
 app.get("/",(req,res,next)=>{
     console.log("/호출");
     res.send("클라이언트에게 보내기");
@@ -42,11 +94,6 @@ app.get("/",(req,res,next)=>{
 
 app.get("/my",mymid)
 
-
-app.post("/subscribe",(req,res,next)=>{
-    console.log(req.body);
-    res.send("구독성공");
-})
 
 app.listen(8080,()=>{
     console.log("서버 8080시작");
