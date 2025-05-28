306a8fe87bf3d8438a7351cf908447859f5cbded
DESKTOP-29VI7GO\user <haracedaily>
2025-05-28 11:22:11 +0900

    no message

--- node_work/back02/app.js
+++ node_work/back02/app.js
@@ -45,7 +45,7 @@ const ss = [];
 // 스케쥴러로 send
 app.post("/subscribe",async (req,res,next)=>{
 
-    // console.log(req.body.sub);
+    console.log(req.body);
     // console.log(req.body.sub.endpoint);
     // console.log(req.body.sub.keys.p256dh);
     // console.log(req.body.sub.keys.auth);
