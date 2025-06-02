1d16729096f4eebaf7ecd4f2ac0b21191c392f2e
DESKTOP-29VI7GO\user <nj7001@naver.com>
2025-06-02 10:31:36 +0900

    no message

--- node_work/back03/app.js
+++ node_work/back03/app.js
@@ -1,13 +1,33 @@
-const express = require("express");
-const path = require("path");
+const express = require("express"); //express라이브러리 역할 app
+const path = require("path");// 경로기반
 const app = express();
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const dotenv = require("dotenv");
-const expressSession = require("express-session");
-const cors = require("cors");
+const morgan = require("morgan"); // 로그
+const cookieParser = require("cookie-parser"); // 쿠키 설정
+const dotenv = require("dotenv"); // .env 파일 process
+const expressSession = require("express-session"); // 세션 설정
+const cors = require("cors"); // 크로스 오리진
+const fs = require("fs"); //파일 시스템
+
+try{
+    fs.readdirSync("uploads");
+}catch (e) {
+    console.error(e);
+    console.error("uploads 폴더가 없어서 uploads폴더 생성합니다.")
+    fs.mkdirSync("uploads");
+}
+
+
+// 넌적스 요청
+const nunjucks = require("nunjucks");
+
+//
+const multer = require("multer");
+
+//.env 환경변수 불러옴
 dotenv.config();
-app.use(morgan("combined"));
+
+
+
 app.use('/',express.static(path.join(__dirname, "public")));
 app.use(express.json());//req.body 확인
 app.use(express.urlencoded({extended:false}));//req.query 확인
@@ -15,6 +35,22 @@ app.use(cors({
     origin:"http://localhost:5174",
     credentials:true,
 }));
+
+// app.use(morgan("combined"));
+// 미들웨어 사용 다른 방법
+app.use((req,res,next)=>{
+    if(process.env.NODE_ENV === "production")
+    morgan("combined")(req,res,next);
+    else
+        morgan("dev")(req,res,next);
+})
+app.use((req,res,next)=>{
+    console.log("밖에서 req 사용가능");
+    (function(req,res,next){
+        next();
+        console.log("req사용가능");
+    })(req,res,next);
+})
 //createContext 모든 컴포넌트에서 속성값 사용 가능, useContext
 app.use(cookieParser(process.env.COOKIE_SECRET));
 //withCredential:true 쿠키전송 여부
@@ -37,6 +73,77 @@ app.use(
         //세션은 메모리가 계속 증가함 => 토큰으로 변경됨
     })
 )
+
+/*
+app.use((req,res,next)=>{
+    console.log("그 다음 미들웨어로 진행",req.method);
+    next();
+},(req,res,next)=>{
+    console.log("다음 미들웨어",req.headers);
+    try{
+        aset
+    }catch(e){
+        next(e);
+    }
+    next();
+},(req,res,next)=>{
+    console.log("다다음 미들웨어",req.uri);
+    next();
+});
+*/
+
+app.set("view engine","html");
+nunjucks.configure(path.join(__dirname,"./views"),{
+    express:app,
+    watch:true,
+})
+
+const upload = multer({
+    storage:multer.diskStorage({//파일은 디스크에 저장하겠다.
+        destination(req,file,done){
+            done(null,"uploads/");//파일이 업로드 되면 uploads경로에 저장하겠다.
+        },
+        // 저장할때 파일 명은
+        filename(req,file,done){
+            const ext = path.extname(file.originalname);
+            console.log(ext);
+            console.log(path.basename(file.originalname,ext)+Date.now()+ext);
+            //원래 파일명 + 현재시간 + 확장자로 설정
+            done(null,path.basename(file.originalname,ext)+Date.now()+ext);
+        },
+    }),
+    limits:{fileSize:100*1024*1024}
+});
+
+app.get("/", (req, res) => {
+    console.log("/경로 요청");
+    console.log(req.query);
+    console.log(req.body);
+    res.locals.member = [{name:"홍길동",age:20},{name:"차길동",age:25}]
+    res.locals.data="새로운 데이터";
+    res.locals.aaa="aaa 데이터";
+    //render는 nunjuscks의 환경설정 해둔 경로의 파일을 보냄
+    res.render("index",{title:"TITLE",data:"?"});//확장자 제외는 view engine에 html로 지정해뒀기 때문
+});
+
+app.get("/multipart",(req,res,next)=>{
+    res.render('multipart');
+})
+//upload.fields([{name:""},{name:""}])
+//nonde 파일 없는 경우
+app.post("/upload",upload.any(),(req,res,next)=>{
+    console.log("upload 들어옴");
+    console.log(req.files,req.body);
+    res.send("저장성공");
+})
+
+app.post("/uploads",upload.array("many"),(req,res,next)=>{
+    console.log("파일 여러개");
+    console.log(req.file);
+    console.log(req.files,req.body);
+    res.send("저장 성공");
+})
+
 // 쿠키는 백엔드에서 생성 프론트에 전달
 // 프론트는 쿠키 저장소에 넣어두고 주소 호출때 자동으로 전달
 app.post("/setSession",(req,res,next)=>{
@@ -114,12 +221,12 @@ app.get("/getCookie",(req,res,next)=>{
     res.send("쿠키확인");
 })
 
-app.get("/", (req, res) => {
-    console.log("/경로 요청");
-    console.log(req.query);
-    console.log(req.body);
-    res.send("Success");
-});
+// app.get("/", (req, res) => {
+//     console.log("/경로 요청");
+//     console.log(req.query);
+//     console.log(req.body);
+//     res.send("Success");
+// });
 
 app.get("/html",(req,res,next)=>{
     res.sendFile(path.join(__dirname,"./index.html"));
@@ -131,8 +238,8 @@ app.get("/html",(req,res,next)=>{
 });*/
 app.use((error, req, res, next)=>{
     console.log("에러 : ",error);
-    console.log(typeof error);
-    res.send("???");
+    console.log("에러 미들웨어 동작");
+    res.send(error.toString());
 })
 app.listen(3000, () => {
     console.log("서버포트 3000번 시작");
