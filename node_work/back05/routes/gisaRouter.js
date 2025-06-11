64cb2754c665fd7daafa04af067ae70b54d00dfb
DESKTOP-29VI7GO\user <haracedaily>
2025-06-11 17:27:54 +0900

    no message

--- node_work/back05/routes/gisaRouter.js
+++ node_work/back05/routes/gisaRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const {supabase} = require('../config/supa.js'); // supabase 클라이언트 설정 파일을 불러옵니다.
 const router = express.Router();
-
-router.get('', async (req, res, next) => {
+/*
+router.route("")
+    .get(async (req, res, next) => {
     console.log("이쪽으로 옴");
     const {data,error} = await supabase.from("ice_res").select();
     console.log(data);
@@ -10,6 +11,29 @@ router.get('', async (req, res, next) => {
     res.render('gisa',{data});
     else
     res.send("데이터를 가져오지 못했습니다.");
-});
+})
+    .post(async (req, res, next) => {
+        console.log(req.body);
+        return res.send("저장됨");
+    });*/
 
+router.get("",async (req, res, next) => {
+        console.log("이쪽으로 옴");
+        const {data,error} = await supabase.from("ice_res").select();
+        console.log(data);
+        if(!error)
+            res.render('gisa',{data});
+        else
+            res.send("데이터를 가져오지 못했습니다.");
+    })
+router.post("",async (req, res, next) => {
+    console.log(req.body);
+    return res.send("저장됨");
+});
+router.post("/pick", async (req, res, next) => {
+    console.log("선택된 데이터:", req.body);
+
+
+    res.send("선택된 데이터가 서버로 전송되었습니다. " + JSON.stringify(req.body));
+});
 module.exports = router;
