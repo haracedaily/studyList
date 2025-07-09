868d350256949e468b2042a502aedab2599ece36
DESKTOP-29VI7GO\user <nj7001@naver.com>
2025-07-09 14:37:51 +0900

    no message

--- node_work/backrncs/routes/index.js
+++ node_work/backrncs/routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/', function (req, res) {
     res.status(200).json({
-        message: 'Welcome to the API',
+        message: '배불러.. 집 가고 싶다..',
         status: 'success'
     });
 })
